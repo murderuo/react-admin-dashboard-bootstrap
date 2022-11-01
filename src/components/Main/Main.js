8f153f26173ebd10c0eb533d6f2bbb7ee7f8b240
murderuo <ugurokur365@hotmail.com>
2022-11-01 16:29:18 +0300

    upload

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <div className="container h-100 my-md-5">
+      <div className="row rounded bg-light" style={{'height':'600px'}}>
+        <div className="col-md-3 bg-light rounded">test</div>
+        <div className="col-md-9 bg-1 rounded">test2</div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
